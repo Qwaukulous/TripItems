d324bf0f9168b0650811c272ce3988b8bf0a7e99
Milana Campbell <milana.campbell@gmail.com>
2019-05-02 15:32:40 -0400

    Add files via upload

---
+++ Pants.java
@@ -0,0 +1,81 @@
+package com.company;
+
+public class Pants {
+
+    public String getBelt() {
+        return belt;
+    }
+
+    public void setBelt(String belt) {
+        this.belt = belt;
+    }
+
+    public String getBuckle() {
+        return buckle;
+    }
+
+    public void setBuckle(String buckle) {
+        this.buckle = buckle;
+    }
+
+    public String getBellbottoms() {
+        return bellbottoms;
+    }
+
+    public void setBellbottoms(String bellbottoms) {
+        this.bellbottoms = bellbottoms;
+    }
+
+    public String getCorduroys() {
+        return corduroys;
+    }
+
+    public void setCorduroys(String corduroys) {
+        this.corduroys = corduroys;
+    }
+
+    public String getLeatherpants() {
+        return leatherpants;
+    }
+
+    public void setLeatherpants(String leatherpants) {
+        this.leatherpants = leatherpants;
+    }
+
+    public String getJeans() {
+        return jeans;
+    }
+
+    public void setJeans(String jeans) {
+        this.jeans = jeans;
+    }
+
+    public String getLevis() {
+        return Levis;
+    }
+
+    public void setLevis(String levis) {
+        Levis = levis;
+    }
+
+    public String getApplebottoms() {
+        return applebottoms;
+    }
+
+    public void setApplebottoms(String applebottoms) {
+        this.applebottoms = applebottoms;
+    }
+
+    private String belt;
+    private String buckle;
+    private String bellbottoms;
+    private String corduroys;
+    private String leatherpants;
+    private String jeans;
+    private String Levis;
+    private String applebottoms;
+
+    public Pants() {
+    }
+}
+
